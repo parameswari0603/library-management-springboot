9e5bfab69f490f73ea120fb5e1672d9b1d3071b7
Parameswari P <parameswarip369@gmail.com>
2026-06-25 15:24:16 +0530

    Day 10 Spring Boot Library App - CRUD completed

--- src/main/java/org/example/libraryapp/LibraryAppApplication.java
+++ src/main/java/org/example/libraryapp/LibraryAppApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LibraryappApplication {
+public class LibraryAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LibraryappApplication.class, args);
+		SpringApplication.run(LibraryAppApplication.class, args);
 	}
-
 }
--- src/main/java/org/example/libraryapp/controller/BookController.java
+++ src/main/java/org/example/libraryapp/controller/BookController.java
@@ -1,7 +1,9 @@
 package org.example.libraryapp.controller;
 
 import org.example.libraryapp.entity.Book;
+import org.example.libraryapp.exception.ResourceNotFoundException;
 import org.example.libraryapp.repository.BookRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,8 +23,30 @@ public class BookController {
         return bookRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable Long id) {
+        return bookRepository.findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException(
+                                "Book not found with id " + id));
+    }
+
     @PostMapping
-    public Book createBook(@RequestBody Book book) {
-        return bookRepository.save(book);
+    public ResponseEntity<Book> createBook(@RequestBody Book book) {
+        Book savedBook = bookRepository.save(book);
+        return ResponseEntity.status(201).body(savedBook);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteBook(@PathVariable Long id) {
+
+        Book book = bookRepository.findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException(
+                                "Book not found with id " + id));
+
+        bookRepository.delete(book);
+
+        return ResponseEntity.ok("Book deleted successfully");
     }
 }
